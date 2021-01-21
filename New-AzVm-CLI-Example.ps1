75e950c83becd987877c15f17ab68a6bb72cba18
alexsarn0 <52964687+alexsarn0@users.noreply.github.com>
2021-01-20 22:17:08 -0600

    Add files via upload

---
+++ New-AzVm-CLI-Example.ps1
@@ -0,0 +1,6 @@
+az vm create \
+    --resource-group TestResourceGroup \
+    --name test-wp1-eus-vm \
+    --image win2016datacenter \
+    --admin-username jonc \
+    --admin-password aReallyGoodPasswordHere
---
+++ Register Resource Provider.ps1
@@ -0,0 +1,2 @@
+# Register the resource provider if it's not already registered
+Register-AzResourceProvider -ProviderNamespace 'Microsoft.PolicyInsights'
